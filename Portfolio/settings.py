105f8a285d7c9477cfe42b5030e87002a09a014a
sujalguy <gajbhiyesujal2003@gmail.com>
2025-11-26 14:44:38 +0530

    Stop tracking .env

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +131,10 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="sujalgajbhiye10@gmail.com"
-EMAIL_HOST_PASSWORD="ygpzmsoiqfrcmxbw"
+EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER")
+
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD")
+
+
+
 
