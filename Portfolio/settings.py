01586c4822eb306ae0fcae2e0b65895a425f2092
sujal gajbhiye <149804805+sujalguy@users.noreply.github.com>
2025-11-27 13:18:53 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    BASE_DIR / "Website/static",
+    BASE_DIR / "static",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
